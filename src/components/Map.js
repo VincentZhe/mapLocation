307843cf7bac9fb1940d217a326c6ff2d7b33f27
Your Name <you@example.com>
2022-05-07 00:31:00 -0400

    my location update

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,29 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../components/Map.css";
 
 const Map = () => {
   const [currentLocation, setCurrentLocation] = useState(null);
+  const [details, setDetails] = useState(null);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setCurrentLocation(position.coords);
+    });
+  }, []);
+  console.log(currentLocation);
 
   const getCurrentLocation = () => {
     fetch(
-      "https://geolocation-db.com/json/fb79dea0-caa7-11ec-aad1-5d8adccafc58"
+      `http://api.timezonedb.com/v2.1/get-time-zone?key=VVW6BIP6JLN5&format=json&by=position&lat=${currentLocation.latitude}&lng=${currentLocation.longitude}`
     )
       .then((res) => res.json())
-      .then((data) => setCurrentLocation(data));
+      .then((data) => setDetails(data));
   };
+  console.log(details);
   return (
     <div className="map-app">
       <div className="map-header">
         <div className="user-location">
           <button onClick={getCurrentLocation}>My Location</button>
           <div className="detail">
-            {currentLocation && (
+            {details && (
               <ul className="list-group">
-                <li>
-                  location :{" "}
-                  {`${currentLocation.city}, ${currentLocation.country_name}`}
-                </li>
-                <li>IP : {currentLocation.IPv4}</li>
+                <li>location : {details.cityName} </li>
+                <li>IP : {details.countryName}</li>
               </ul>
             )}
           </div>
@@ -34,9 +40,6 @@ const Map = () => {
         </div>
       </div>
       <div className="map-content">
-        <div className="map-table">table</div>
-        <div className="map-table">table</div>
-        <div className="map-table">table</div>
         <div className="map-table">table</div>
         <div className="map">map</div>
       </div>
